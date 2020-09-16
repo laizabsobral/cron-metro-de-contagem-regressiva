2faca13576b6e8ff5b94a6a147033887cdeeb276
Laiza Benevides <laizabsobral@gmail.com>
2020-09-16 11:04:26 -0300

    Novas alterações

---
+++ script.js
@@ -0,0 +1,30 @@
+const diasEL = document.getElementById('Dias');
+const horasEL = document.getElementById('Horas');
+const minutosEL = document.getElementById('Min');
+const segundosEL = document.getElementById('Segundos');
+
+
+
+const anoNovo = '01 de Janeiro de 2021';
+
+function contador () {
+    const anoNovoDate = new Date(anoNovo);
+    const currentDate = new Date();
+
+    const totalSegundos =(anoNovoDate - currentDate) * 1000;
+
+    const dias = Math.floor(totalSegundos / 3600 / 24);
+    const horas = Math.floor(totalSegundos/ 3600) % 24;
+    const minutos = Math.floor(totalSegundos/60) % 60;
+    const segundos = Math.floor(totalSegundos) % 60;
+
+    diasEL.innerHTML = dias;
+    horasEL.innerHTML = horas;
+    minutosEL.innerHTML = minutos;
+    segundosEL.innerHTML = segundos;
+
+}
+
+contador();
+
+setInterval(contador,1000);
